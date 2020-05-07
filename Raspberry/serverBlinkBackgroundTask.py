0dd03f86ffd007893a962af7ead830d17872f860
Ankur Nigam <ankur.nigam198@gmail.com>
2020-05-07 08:48:48 +0530

    backgroundTask

--- Raspberry/serverBlinkBackgroundTask.py
+++ Raspberry/serverBlinkBackgroundTask.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 from time import sleep
+from threading import Thread
 from flask import Flask
 
 GPIO.setwarnings(False)
@@ -9,6 +10,7 @@ GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(10, GPIO.OUT, initial=GPIO.LOW)
 
 app = Flask(__name__)
+discoThread = None
 
 
 @app.route('/')
@@ -22,6 +24,26 @@ def change_pin_state(pin_num, value):
         GPIO.output(pin_num, value)
         return f'{pin_num} {value}'
     return 'bad request'
+
+
+@app.route('/disco/<int:value>')
+def disco(value):
+    global discoThread
+    if value == 1:
+        discoThread = Thread(target=blink)
+        discoThread.start()
+    elif discoThread != None:
+        discoThread._stop()
+
+
+def blink():
+    while True:
+        GPIO.output(8, GPIO.HIGH)  # Turn on
+        GPIO.output(10, GPIO.LOW)  # Turn on
+        sleep(1)  # Sleep for 1 second
+        GPIO.output(8, GPIO.LOW)  # Turn off
+        GPIO.output(10, GPIO.HIGH)  # Turn off
+        sleep(1)  # Sleep for 1 second
 
 
 if __name__ == "__main__":
