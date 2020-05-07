15676c216754e82ebca5613aec40de41797ac2d2
Ankur Nigam <ankur.nigam198@gmail.com>
2020-05-07 11:05:50 +0530

    ui

--- Raspberry/fullDemo/static/script.js
+++ Raspberry/fullDemo/static/script.js
@@ -4,3 +4,16 @@ async function getStatus() {
   const res = await fetch(serverUrl + "/status");
   return res.json();
 }
+
+async function onButtonChanged(element) {
+  if (element.id === "red") {
+    await fetch(serverUrl + "/10/" + element.checked ? "1" : "0");
+  }
+  console.log(element.id, element.checked);
+}
+
+function onLoaded() {
+  document.querySelectorAll(".button input[type=checkbox]").forEach((x) => {
+    x.addEventListener("click", (y) => onButtonChanged(x));
+  });
+}
