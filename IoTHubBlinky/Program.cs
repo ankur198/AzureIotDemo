bf2b991048cf71d5e3b6a88bce2ae905b6f730ab
Ankur Nigam <ankur.nigam198@gmail.com>
2020-05-08 10:47:07 +0530

    gpio blink

--- IoTHubBlinky/Program.cs
+++ IoTHubBlinky/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Azure.Devices.Client;
 using Newtonsoft.Json;
 using System;
+using System.Device.Gpio;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace IoTHubBlinky
@@ -14,11 +16,24 @@ namespace IoTHubBlinky
         // az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyDotnetDevice --output table
         private readonly static string s_connectionString = "HostName=demoHub198.azure-devices.net;DeviceId=blinkyDotnet;SharedAccessKey=4Lz3/WcLZAUIlcS8Cpl3sTtKLgRk+Ia8Lr5iME5Voac=";
 
+        private static GpioController gpioController;
+        private static readonly int ledPin = 8;
+
         private static async Task Main(string[] args)
         {
             s_deviceClient = DeviceClient.CreateFromConnectionString(s_connectionString);
 
-            Console.WriteLine("Hello World!");
+            try
+            {
+                gpioController = new GpioController();
+                gpioController.OpenPin(ledPin, PinMode.Output);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error connecting to gpio controller. " + e.Message);
+            }
+
+            Console.WriteLine("Device ready");
 
             RecieveMessage();
 
@@ -50,9 +65,73 @@ namespace IoTHubBlinky
             {
                 var message = await s_deviceClient.ReceiveAsync();
 
-                Console.WriteLine($"Message recieved... {Encoding.ASCII.GetString(message.GetBytes())}");
+                var messageString = Encoding.ASCII.GetString(message.GetBytes());
+
+                Console.WriteLine($"Message recieved... {messageString}");
+
+                if (gpioController != null)
+                {
+                    switch (messageString)
+                    {
+                        case "on":
+                            turnOn();
+                            break;
+
+                        case "off":
+                            turnOff();
+                            break;
+
+                        case "disco":
+                            toggleDisco();
+                            break;
+
+                        default:
+                            Console.WriteLine("No actions specified");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("GpioController not found...Can't perform actions.");
+                }
 
                 await s_deviceClient.CompleteAsync(message);
+            }
+        }
+
+        private static void turnOn()
+        {
+            gpioController.Write(ledPin, PinValue.High);
+        }
+
+        private static void turnOff()
+        {
+            gpioController.Write(ledPin, PinValue.Low);
+        }
+
+        private static bool isDiscoStarted = false;
+
+        private static void toggleDisco()
+        {
+            if (isDiscoStarted)
+            {
+                isDiscoStarted = false;
+            }
+            else
+            {
+                isDiscoStarted = true;
+                startDisco();
+            }
+        }
+
+        private static async Task startDisco()
+        {
+            while (isDiscoStarted)
+            {
+                gpioController.Write(ledPin, PinValue.High);
+                Thread.Sleep(1000);
+                gpioController.Write(ledPin, PinValue.Low);
+                Thread.Sleep(1000);
             }
         }
     }
