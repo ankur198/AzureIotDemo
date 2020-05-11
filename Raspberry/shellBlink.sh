833f98a519f7794595240499a0f8022a7923a20d
Ankur Nigam <ankur.nigam198@gmail.com>
2020-05-11 09:30:30 +0530

    fixed while loop syntax

--- Raspberry/shellBlink.sh
+++ Raspberry/shellBlink.sh
@@ -1,10 +1,10 @@
 gpio -1 mode 10 out
 gpio -1 mode 8 out
 
-while true
-do:
+while :
+do
     gpio -1 write 10 1
-    gpio -1 write 8 0
+    gpio -1 write 8 0 
     sleep 1
     gpio -1 write 10 0
     gpio -1 write 8 1
